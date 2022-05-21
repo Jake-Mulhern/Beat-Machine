3b89feca2ac2eae3fa23e476d36c84df5fc3a939
Jacob Mulhern <jacobmulhern@Jacobs-MBP.lan>
2022-05-20 18:13:00 -0700

    Clean Up

--- index.py
+++ index.py
@@ -290,33 +290,34 @@ while run:
                 playing = True
                 beat_name = ''
                 typing = False
-            elif loaded_rectangle.collidepoint(event.pos):
-                index = (event.pos[1] - 100) // 50
-            elif delete_btn.collidepoint(event.pos):
-                if 0 <= index < len(saved_beats):
-                    saved_beats.pop(index)
-            elif loading_btn.collidepoint(event.pos):
-                if 0 <= index < len(saved_beats):
-                    beats = loaded_info[0]
-                    bpm = loaded_info[1]
-                    clicked = loaded_info[2]
-                    index = 100
-                    load_menu = False
-
-            elif entry_rectangle.collidepoint(event.pos):
-                if typing:
+            if load_menu:
+                if loaded_rectangle.collidepoint(event.pos):
+                    index = (event.pos[1] - 100) // 50
+                if delete_btn.collidepoint(event.pos):
+                    if 0 <= index < len(saved_beats):
+                        saved_beats.pop(index)
+                if loading_btn.collidepoint(event.pos):
+                    if 0 <= index < len(saved_beats):
+                        beats = loaded_info[0]
+                        bpm = loaded_info[1]
+                        clicked = loaded_info[2]
+                        index = 100
+                        load_menu = False
+            if save_menu:
+                if entry_rectangle.collidepoint(event.pos):
+                    if typing:
+                        typing = False
+                    elif not typing:
+                        typing = True
+                if saving_btn.collidepoint(event.pos):
+                    file = open('my_beats.txt', 'w')
+                    saved_beats.append(f'\nname: {beat_name}, beats: {beats}, bpm: {bpm}, selected: {clicked}')
+                    for i in range(len(saved_beats)):
+                        file.write(str(saved_beats[i]))
+                    file.close()
+                    save_menu = False
                     typing = False
-                elif not typing:
-                    typing = True
-            elif saving_btn.collidepoint(event.pos):
-                file = open('my_beats.txt', 'w')
-                saved_beats.append(f'\nname: {beat_name}, beats: {beats}, bpm: {bpm}, selected: {clicked}')
-                for i in range(len(saved_beats)):
-                    file.write(str(saved_beats[i]))
-                file.close()
-                save_menu = False
-                typing = False
-                beat_name = ''
+                    beat_name = ''
 
         if event.type == pygame.TEXTINPUT and typing:
             beat_name += event.text
